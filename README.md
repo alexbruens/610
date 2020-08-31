70de3ad0e49dfbe421531ed50ad2fd30e16b3868
Alex Bruens <alexbruens@gmail.com>
2020-08-31 13:41:35 -0700

    Edits

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 610
+# 610 Materials
+
+Course materials for POL 610 -- Network analysis
--- lab1script.R
+++ lab1script.R
@@ -3,7 +3,5 @@
 # Probelm Set/Lab 1 Script
 # August 2020
 
-<<<<<<< HEAD
 load("./hRU_v2019_04_17.RData")
-=======
->>>>>>> 55c0cbaedff462efa70656b790944477b9f1d943
+
