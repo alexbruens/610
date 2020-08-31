7821be2a3cb32e1da6ae0142afe57aef8c6c1bb0
Alex Bruens <alexbruens@gmail.com>
2020-08-31 14:18:32 -0700

    Load network data

--- lab1script.R
+++ lab1script.R
@@ -3,5 +3,9 @@
 # Probelm Set/Lab 1 Script
 # August 2020
 
+library("readxl")
 load("./hRU_v2019_04_17.RData")
 
+politicalnetworks <- read_excel("politicalnetworkmatrix.xlsx")
+
+ideology <- matrix()
